1fa48e785cf331afbce1fff096dc641394e09856
Tejas Rane <rtejas621@gmail.com>
2025-12-07 22:16:36 +0530

    Vite

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,14 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
   return {
-   base: mode === "development" ? "/" : `/${env.VITE_REPO_NAME}/`,
+    base: "/",   // ✔ FIXED — Netlify needs this!
 
     plugins: [react(), tailwindcss()],
     server: {
       open: true,
-      host: false, // Allow access from network devices
+      host: false,
       proxy: {
         "/api": {
           target: "http://localhost:5000",
