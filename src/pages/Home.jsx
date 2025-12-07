0442d32cb47fbe29eec483488b3117fc68f9fdbf
Tejas Rane <rtejas621@gmail.com>
2025-12-07 14:48:37 +0530

    Fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import Introduction from "../components/introduction/Introduction";
 import Profile from "../components/profile/Profile";
-import WorkProcess from "../components/workProcess/Skills";
+import Skills from "../components/workProcess/Skills";
 import Portfolio from "../components/portfolio/Portfolio";
 import WorkTogether from "../components/workTogether/WorkTogether";
 import Blog from "../components/blog/Blog";
@@ -21,7 +21,7 @@ const Home = () => {
         </div>
       </div>
       <div className="bg-soft-white pt-30">
-        <WorkProcess />
+        <Skills />
       </div>
       <Portfolio />
       <div className="bg-gray-900">
